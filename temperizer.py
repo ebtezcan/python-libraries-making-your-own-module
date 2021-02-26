4873f5f2c454c9c937966f4a7c63cf27c3644c23
Berke Tezcan <berketezcan@gmail.com>
2021-02-26 14:47:13 -0500

    Added Conversion to F from C

--- temperizer.py
+++ temperizer.py
@@ -1,5 +1,6 @@
 """An example library for converting temperatures."""
 
+
 def convert_f_to_c(temperature_f):
     """Convert Fahrenheit to Celsius."""
     temperature_c = (temperature_f - 32) * (5/9)
@@ -7,5 +8,6 @@ def convert_f_to_c(temperature_f):
 
 
 def convert_c_to_f(temperature_c):
-    """YOUR DOCSTRING HERE."""
-    pass
+    """Convert Celsius to Fahrenheit"""
+    temperature_f = (temperature_c × 9/5) + 32)
+    return temperature_f
